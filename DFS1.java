6b26984ca45fea6e08eac308680895044da8b418
obidkor <coavldjs93@naver.com>
2024-05-10 22:50:10 +0900

    Update DFS1.java

--- DFS1.java
+++ DFS1.java
@@ -6,7 +6,7 @@ import java.io.*;
 /* DFS  : https://wikidocs.net/146572 */
 class Main {
     public static void main(String[] args) {
-        // 트리생성
+        //1. 트리 생성
         int[][] map= {
                 {0,1},
                 {0,2},
@@ -17,50 +17,38 @@ class Main {
                 {4,5},
                 {5,6}
         };
+        //2. 필요한 변수 생성
+        Stack<Integer> st=new Stack<Integer>();
 
-        // 필요한 변수 생성
-        Stack<Integer> st = new Stack<Integer>();
-        int[] visited = new int[7];
-
-        // 초기값 입력
-        int startNode = 0;
+        //3. 초기값 입력
+        int startNode=0;
         st.push(startNode);
-        visited[startNode] = 1;
 
-        //DFS
-        while(!st.isEmpty())
-        {
-            // Stack에 가장 위의 값을 pop
-            int now = st.pop();
-
-            // 연결된 노드를 찾아 Stack에 저장 및 visited에 표기
-            for(int i = 0; i < map.length; i++)
-            {
-                if(map[i][0] == now)    
-                {
-                    int next = map[i][1];
-                    if(visited[next] == 0)
-                    {
-                        st.push(next);
-                        visited[next] = visited[now] + 1;
+        //4. DFS
+        while(!st.isEmpty()) {
+            //4-1. Stack에 가장 위의 값을 pop
+            int now=st.pop();
+            //4-2. 연결된 노드를 찾아 Stack에 저장 및 visited에 표기
+            for(int i=0;i<map.length;i++) {
+                if(map[i][0]==now) {
+                    int next=map[i][1];
+                    st.push(next);
                     }
-                    
                 }
-            }
-
-            System.out.println("pop : " + now);
-            System.out.println("Stack : " + st.toString());
-            System.out.println(Arrays.toString(visited));
-            System.out.println("========================");
+            System.out.println("pop : "+ now);
+            System.out.println("Stack : "+st.toString());
+            System.out.println("-----------------------");
+            //출력결과
+            //pop : 0
+            //Stack : [1, 2]
+            // ...(중략)...
+            //pop : 6
+            //Stack : []
+            //
         }
-        
-
-
-        
     }
 
-
-       // visited 구현
+       // visited를 구현하여 방문 순서출력
         public static void main(String[] args) {
         // 트리생성
         int[][] map= {
